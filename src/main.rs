e70d01c76a7a4377edb3173c9db6b653bd3f2ab9
Gustavo Batistela <gustavoabatistela@gmail.com>
2023-10-02 16:41:59 -0300

    feat: read external config file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 //! main.rs
 use std::net::TcpListener;
+use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use std::net::TcpListener;
-use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
